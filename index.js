e0595e67b3a132bcb299d55e89565e6450b033e5
Steve Pepple <stephen.pepple@gmail.com>
2020-06-27 17:15:12 -0700

    Try calling build/server from index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+let app = require('build/server.js').default;
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+try {
+    app = require('./server').default;
+} catch (error) {
+    console.error(error);
+}
